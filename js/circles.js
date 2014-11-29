80623a2acc9c718fd77cbb9c14ac4895d9023147
Jody Hamilton <jody@zivtech.com>
2014-11-28 18:24:48 -0800

    starting circles game

---
+++ js/circles.js
@@ -0,0 +1,28 @@
+$(function() {
+  $("body").click(function() {
+    addRandomCircle();
+  });
+});
+
+function addRandomCircle() {
+  var styles = getRandomStyles();
+  $("#circles").append("<div class='circle' style='"+styles+"'></div>");
+}
+
+function getRandomStyles() {
+  var width = $(window).width() - 200;
+  var slots = Math.floor(width/50);
+  var horizontalOffset = Math.floor((Math.random() * slots) + 1) * 50;
+  var color = getRandomColor();
+  var sizes = [50, 100, 150, 200];
+  var size = sizes[Math.floor(Math.random()*sizes.length)];
+  var verticalOffset = (200 - size)/2;
+  var styles = "left:"+horizontalOffset+"px;background-color:"+color+";top:"+verticalOffset+"px;width:"+size+"px;height:"+size+"px;";
+  console.log(styles);
+  return styles;
+}
+
+function getRandomColor() {
+  var colors = ['#d10000', '#ff6622', '#ffda21', '#33dd00', '#1133cc', '#220066', '#330044'];
+  return colors[Math.floor(Math.random()*colors.length)];
+}
